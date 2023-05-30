0a163ddb658b58d0ed22a79a0c3f26e1fc19077f
Eurico <eurico.neto6@gmail.com>
2023-05-30 15:38:07 -0300

    Implementando as classes de teste

--- src/conversor/ConversorMoeda/ConversorDolar.java
+++ src/conversor/ConversorMoeda/ConversorDolar.java
@@ -2,15 +2,25 @@ package conversor.ConversorMoeda;
 
 public class ConversorDolar implements ConversorMoeda{
 
+
+
+
     @Override
     public double ConverterDeReal(double value) {
         return value * 4.9;
     }
 
+
     @Override
     public double converterParaReal(double value) {
         return value * 0.20;
     }
+
+
+   
+
+    
+
 
     
 
